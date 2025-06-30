adefbb284a220e8faaa51a6634a15519fd6cd638
heinthuhtet404 <heinthuhtet404@gmail.com>
2025-07-01 01:14:52 +0630

    fix : adding dynamic route and error page

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,21 @@ import Main from './layouts/Main.jsx'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
 import Products from './pages/Products.jsx'
+import Error from './pages/Error.jsx'
+import Product from './pages/Product.jsx'
 
 const router = createBrowserRouter([
   {
     path: '',
     element: <Main />,
+    errorElement: <Error/>,
+    // This is the default route, it will be used if no path is specified
     children: [
       { path: '/', element: <Home /> },
       { path: '/about', element: <About /> },
-      { path: '/products', element: <Products /> }
+      { path: '/products', element: <Products /> },
+      { path: '/product/:id', element: <Product/> },
+        // This is a dynamic route, it will match any path like /product/1,
     ]
   }
 ])
